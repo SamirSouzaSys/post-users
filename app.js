7ca92cf68db2107e8455225cfc2d3fc995970392
samirsouzasys <samir.guitar@gmail.com>
2024-02-17 00:01:05 -0300

    aula 3

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const mongoose = require('mongoose')
 
 const app = express()
 
@@ -6,5 +7,27 @@ app.get("/:nome", (req, res) => {
   // res.send('Hello world')
   res.send("Hello " + req.params.nome)
 })
+
+mongoose.connect(
+  "mongodb+srv://samirguitar:QtfX9VO3aF5FWFjP@cluster0.8esoif9.mongodb.net/?retryWrites=true&w=majority"
+//   ,{
+    // useNewUrlParser: true,
+    // useFindAnModify: true}
+)
+
+// async function testar () {}
+const testar = async () => {
+  const Test = mongoose.model("Test", { name: String })
+
+  const t = new Test({
+    name: "Testando mongoose novamente",
+  })
+
+  await t.save().then(() => {
+    console.log('Objeto criado')
+  })
+}
+
+testar()
 
 app.listen("4000")
